7800977eb7875742bd3396a07a829bb2320c7f81
David Russell <davidrussell@Davids-MacBook-Pro.local>
2020-04-18 20:22:28 -0500

    adding heroku host

--- csce4523/settings.py
+++ csce4523/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r$8tu634_6)aed8m3vy(08=0x*s-cq7*_doz0e@3^8ej65d)#7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nameless-temple-39549.herokuapp.com']
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
